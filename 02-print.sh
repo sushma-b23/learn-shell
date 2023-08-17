3fdfec2440841efd1c0e73c1c08745b367c9bf46
sushma-b23 <susmaboothu5@gmail.com>
2023-08-17 16:15:35 +0530

    Shell Scripting Topics

--- 02-print.sh
+++ 02-print.sh
@@ -2,19 +2,19 @@ echo Hello World
 
 echo "*** Hello World ***"
 
-# color code syntax : echo -e "\e[COLmMESSAGE\e[om"
-# Red     -31 - echo -e "\e[31mMESSAGE\e[om"
-# Green   -32 - echo -e "\e[32mMESSAGE\e[om"
-# Yellow  -33 - echo -e "\e[33mMESSAGE\e[om"
-# Blue    -34 - echo -e "\e[34mMESSAGE\e[om"
-# Magenta -35 - echo -e "\e[35mMESSAGE\e[om"
-# Cyan    -36 - echo -e "\e[36mMESSAGE\e[om"
+# color code syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# Red     -31 - echo -e "\e[31mMESSAGE\e[0m"
+# Green   -32 - echo -e "\e[32mMESSAGE\e[0m"
+# Yellow  -33 - echo -e "\e[33mMESSAGE\e[0m"
+# Blue    -34 - echo -e "\e[34mMESSAGE\e[0m"
+# Magenta -35 - echo -e "\e[35mMESSAGE\e[0m"
+# Cyan    -36 - echo -e "\e[36mMESSAGE\e[0m"
 
 
 
-echo -e "\e[31mMESSAGE\e[om" 
-echo -e "\e[32mMESSAGE\e[om" 
-echo -e "\e[33mMESSAGE\e[om" 
-echo -e "\e[34mMESSAGE\e[om" 
-echo -e "\e[35mMESSAGE\e[om" 
-echo -e "\e[36mMESSAGE\e[om" 
+echo -e "\e[31mMESSAGE\e[0m"
+echo -e "\e[32mMESSAGE\e[0m"
+echo -e "\e[33mMESSAGE\e[0m"
+echo -e "\e[34mMESSAGE\e[0m"
+echo -e "\e[35mMESSAGE\e[0m"
+echo -e "\e[36mMESSAGE\e[0m"
