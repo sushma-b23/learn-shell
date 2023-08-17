3d87a724ddc761254c0632264e582578a8d93dea
sushma-b23 <susmaboothu5@gmail.com>
2023-08-17 16:27:44 +0530

    Shell Scripting Topics

--- 02-print.sh
+++ 02-print.sh
@@ -18,3 +18,6 @@ echo -e "\e[33mMESSAGE\e[0m"
 echo -e "\e[34mMESSAGE\e[0m"
 echo -e "\e[35mMESSAGE\e[0m"
 echo -e "\e[36mMESSAGE\e[0m"
+
+
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
---
+++ variable.sh
@@ -0,0 +1,7 @@
+## If we assign a name to a set of data is called as Variable.
+
+student=sushma
+class=DevOps
+
+echo Student Name - $student
+echo Class Name - ${class}
