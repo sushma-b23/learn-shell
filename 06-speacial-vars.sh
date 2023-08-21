967d65349ebf97e9bfea34036558dbfec2780e28
sushma-b23 <susmaboothu5@gmail.com>
2023-08-21 18:49:20 +0530

    if.sh

--- 06-speacial-vars.sh
+++ 06-speacial-vars.sh
@@ -4,3 +4,16 @@ echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+
+echo "Following Values from Script"
+example() {
+  echo "Following values from function"
+  echo "0 - $0"
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+example
