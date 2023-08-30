b3c8c8defbb9ce1c884ed56efa1cfe3d3c3492ea
sushma-b23 <susmaboothu5@gmail.com>
2023-08-30 17:26:22 +0530

    if.sh

---
+++ 08-condition.sh
@@ -0,0 +1,22 @@
+read -p 'Enter Your age: ' age
+read -p 'Enter course Name: ' course_name
+case $course_name in
+
+AWS)
+  echo "welcome to AWS Training"
+  ;;
+AZURE)
+  echo "welcome to AZURE Training"
+  ;;
+*)
+  echo "Unknown course Name"
+  ;;
+fi
+
+if [ "${course_name}" == "AWS" ]; then
+echo "welcome to AWS Training"
+elif [ "${course_name}" == "AZURE" ]; then
+echo "welcome to AZURE Training"
+else
+echo "Unknown course Name"
+fi
